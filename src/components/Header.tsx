8742fb06e7a8ce9b7998bd5f84cec128bf517927
Josh Dickinson <josh25dickinson@gmail.com>
2023-09-19 01:16:22 -0500

    add header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
 const Header = () =>{
     return (
-      <div className="mb-128 fixed top-0 left-0 right-0 text-2xl bg-lighterblack text-grey flex flex-row justify-between">
-        <div className="text-white ml-20 my-6 group flex ">
+      <div className="fixed top-0 left-0 right-0 bg-lighterblack text-grey text-md md:text-2xl mx-auto md:flex flex-row justify-between">
+        <div className="ml-10 text-white py-6 group flex justify-center scale-0 md:scale-100">
 
-          <button className="align-middle font-semibold hover:text-white transition-all scale-0 group-hover:scale-100">
-            🦆
+          <button className="align-middle font-semibold hover:text-white transition-all md:scale-0 group-hover:scale-100">
+            🦆&nbsp;
           </button>
-          <a href="#title" className="break-keep align-left font-bold hover:text-white transition-all hover:translate-x-1">
-           &nbsp;Joshua Dickinson
+          <a href="#title" className="align-left font-bold hover:text-white transition-all hover:translate-x-1">
+            Joshua Dickinson
           </a>
           
         </div>
 
-        <div className="text-2xl ml-12 flex flex-row flex-nowrap items-center justify-between">
+        <div className="pb-6 -mt-10 md:mt-6 flex flex-row flex-nowrap items-center justify-center">
           <a href="#about" className="scroll-smooth mx-4 hover:text-white">
               About
           </a>
@@ -22,7 +22,7 @@ const Header = () =>{
           <a href="#contact" className="mx-4 align-middle hover:text-white">
             Contact
           </a>
-          <a href="../resume.pdf" download="Resume - Joshua Dickinson - Fall 2023" className="mr-20 ml-4 align-middle hover:text-white">
+          <a href="../resume.pdf" download="Resume - Joshua Dickinson - Fall 2023" className="mx-4 md:mr-16 align-middle hover:text-white">
             Resume
           </a>
           
