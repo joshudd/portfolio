0f07f51a8e1396a39340034788353de9c33475a8
Josh Dickinson <josh25dickinson@gmail.com>
2024-08-31 14:55:29 -0500

    initial styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Header from '@/components/Header'
-import About from '@/components/About'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
-import Title from '@/components/Title'
+import HomeView from '@/components/views/home-view'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between w-full bg-black">
-      <Header/>
-      <Title/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      <HomeView />
     </main>
   )
 }
---
+++ src/components/views/home-view.tsx
@@ -0,0 +1,11 @@
+import exp from "constants";
+import React from "react";
+
+export default function HomeView() {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+        <text>Home</text>
+        </div>
+    );
+}
+
