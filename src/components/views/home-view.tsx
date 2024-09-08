6f5da242f3d893b1625c0ba5bc78d2bd435a6f1f
Josh Dickinson <josh25dickinson@gmail.com>
2024-09-08 17:12:22 -0500

    hide bottom for mobile

--- src/components/views/home-view.tsx
+++ src/components/views/home-view.tsx
@@ -96,7 +96,7 @@ const HomeView = () => {
             </div>
 
             {/* note */}
-            <div className="absolute bottom-[0vh] p-2 md:p-3 lg:p-4 bg-background-transparent-color rounded-sm md:rounded-lg backdrop-blur-sm z-[200]">
+            <div className="hidden md:block absolute bottom-[0vh] p-2 md:p-3 lg:p-4 bg-background-transparent-color rounded-sm md:rounded-lg backdrop-blur-sm z-[200]">
                 <h2 className="text-[10px]">
                     designed and built by me
                 </h2>
