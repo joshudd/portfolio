d2bd0d60454f575e81c942e1cf467d529beefe12
Josh Dickinson <josh25dickinson@gmail.com>
2024-09-11 23:29:00 -0500

    color + size

--- src/components/views/home-view.tsx
+++ src/components/views/home-view.tsx
@@ -32,7 +32,7 @@ const HomeView = () => {
         <div className="w-full h-screen flex flex-col justify-center items-center text-text-color">
             {/* header */}
             <div className="mb-6 absolute top-[15vh] left-[10vw] p-2 md:p-3 lg:p-4 bg-background-transparent-color rounded-sm md:rounded-lg backdrop-blur-sm z-[200]">
-                <h1 className="text-xl sm:text-2xl md:text-3xl font-normal">
+                <h1 className="text-[26px] sm:text-[28px] md:text-[30px] lg:text-[38px] font-normal">
                     hey there, i&apos;m josh!
                 </h1>
             </div>
@@ -68,7 +68,7 @@ const HomeView = () => {
                         <div className="blur-sm w-24 h-24">
                             {isAboutHovered && (<div className="fade-in"><Ripples view="home"/></div>)}
                         </div>
-                        <h2 className="text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-normal mt-5">
+                        <h2 className="text-[22px] sm:text-[24px] md:text-[26px] lg:text-[32px] font-normal mt-5">
                             about
                         </h2>
                     </a>
@@ -85,7 +85,7 @@ const HomeView = () => {
                         <div className="blur-sm w-24 h-24">
                             {isDesignHovered && (<div className="fade-in"><Ripples view="home"/></div>)}
                         </div>
-                        <h2 className="text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-normal mt-5">
+                        <h2 className="text-[22px] sm:text-[24px] md:text-[26px] lg:text-[32px] font-normal mt-5">
                             design
                         </h2>
                     </a>
@@ -102,7 +102,7 @@ const HomeView = () => {
                         <div className="blur-sm w-24 h-24">
                             {isProjectsHovered && (<div className="fade-in"><Ripples view="home"/></div>)}
                         </div>
-                        <h2 className="text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px] font-normal mt-5">
+                        <h2 className="text-[22px] sm:text-[24px] md:text-[26px] lg:text-[32px] font-normal mt-5">
                             projects
                         </h2>
                     </a>
